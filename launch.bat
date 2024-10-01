14e772e3a15bdd9fb220c421b544e7145fb907c5
Dan Rouse <dan@rouse.house>
2024-10-01 12:10:44 -0500

    add livesplit to launcher script

--- launch.bat
+++ launch.bat
@@ -2,10 +2,17 @@
 tasklist /nh /fi "ImageName eq Streamer.bot.exe" 2>nul | find /i "Streamer.bot.exe">nul
 if "%ERRORLEVEL%" NEQ "0" (
   echo Starting Streamer.bot...
-  start streamer.bot/Streamer.bot.exe
+  start streamer.bot\Streamer.bot.exe
   timeout /T 10 > nul
 ) else (
   echo Streamer.bot is already running, continuing...
+)
+
+tasklist /nh /fi "ImageName eq LiveSplit.exe" 2>nul | find /i "LiveSplit.exe">nul
+if "%ERRORLEVEL%" NEQ "0" (
+  echo Starting LiveSplit, activate the WebSocket server before continuing!
+  start L:\LiveSplitDevBuild\LiveSplit.exe
+  pause
 )
 
 echo Starting music-stem-server...
