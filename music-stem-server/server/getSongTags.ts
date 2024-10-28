3c8de4a68b22424bf6fe94395155c4e355543992
Dan Rouse <dan@rouse.house>
2024-10-28 12:55:06 -0500

    slight rework of song tags parsing

--- music-stem-server/server/getSongTags.ts
+++ music-stem-server/server/getSongTags.ts
@@ -1,5 +1,6 @@
 import ffprobe from 'ffprobe';
 import ffprobeStatic from 'ffprobe-static';
+import { basename } from 'path';
 // @ts-expect-error
 import { parseFile } from 'music-metadata';
 import { parseTime } from '../../shared/util';
@@ -8,25 +9,22 @@ export default async function getSongTags(songPath: string) {
   let tags: any = {};
   try {
     tags = await parseFile(songPath);
-  } catch (e) {
-    const res = await ffprobe(songPath, { path: ffprobeStatic.path });
-
-    let duration = 0;
-    if (res.streams[0].duration) {
-      duration = Number(res.streams[0].duration);
-    } else if (res.streams[0].tags.DURATION) {
-      duration = parseTime(res.streams[0].tags.DURATION);
-    }
-    const partsMatch = songPath.match(/([^-]+) - (.+)$/);
-    tags = {
-      common: {
-        artist: partsMatch?.[1],
-        title: partsMatch?.[2],
-        album: 'YouTube',
-        track: { no: 1, of: 1 },
-      },
-      format: { duration },
-    };
+  } catch (e) {}
+  const res = await ffprobe(songPath, { path: ffprobeStatic.path });
+  let duration = 0;
+  if (res.streams[0].duration) {
+    duration = Number(res.streams[0].duration);
+  } else if (res.streams[0].tags.DURATION) {
+    duration = parseTime(res.streams[0].tags.DURATION);
   }
+  const videoTitle = basename(songPath).replace(/^YouTube - /, '');
+  const partsMatch = videoTitle.match(/(([^-]+) - )?(.+)( \[\S{11}\])\.(...)$/);
+  if (!tags.common) tags.common = {};
+  tags.common.artist ||= partsMatch?.[2] || 'Unknown';
+  tags.common.title ||= partsMatch?.[3];
+  tags.common.album ||= `YouTube${partsMatch?.[4] || ''}`;
+  tags.common.track ||= { no: 1, of: 1 };
+  if (!tags.format) tags.format = {};
+  tags.format.duration ||= duration;
   return tags;
 }
