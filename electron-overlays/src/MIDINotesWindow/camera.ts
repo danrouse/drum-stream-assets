a00a1e912d1fd748bf3aa990db87d457372e9e4a
Dan Rouse <dan@rouse.house>
2024-11-07 13:34:38 -0600

    use specific camera for MIDINotes
    there's probably a better way to introduce UI to select this, but hardcode for now

--- electron-overlays/src/MIDINotesWindow/camera.ts
+++ electron-overlays/src/MIDINotesWindow/camera.ts
@@ -7,10 +7,16 @@ export default async function initializeCamera(parentElem: HTMLElement) {
     parentElem.appendChild(videoElem);
   }
 
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  console.log('Video Devices', devices.filter(d => d.kind === 'videoinput').map(d => [d.label, d.deviceId]));
+
   const cameraStream = await navigator.mediaDevices.getUserMedia({
     video: {
       width: 1920,
       height: 1080,
+      deviceId: {
+        exact: 'abbd7e022d1282d5ba2103ff0e3526c1c9554b957e8513daeebe2fd5c35b83ef',
+      }
     },
     audio: false,
   });
