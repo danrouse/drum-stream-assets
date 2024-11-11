fbd32e26a55906b340976ec9e8a74642ffbae918
Dan Rouse <dan@rouse.house>
2024-11-11 14:21:20 -0600

    remove autogenerated comment that is a lie

--- music-stem-server/player/main.tsx
+++ music-stem-server/player/main.tsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client'
 import SongBrowserUI from './SongBrowserUI.tsx'
 import './index.css'
 
-/**
- * Entry point for the Now Playing UI
- * running on the server host via Electron
- */
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SongBrowserUI />
