40f70de2647ec193ea134f0806020fab97828385
Dan Rouse <dan@rouse.house>
2024-12-05 16:16:46 -0600

    don't launch livesplit in launcher script

--- launch.bat
+++ launch.bat
@@ -8,17 +8,17 @@ if "%ERRORLEVEL%" NEQ "0" (
   echo Streamer.bot is already running, continuing...
 )
 
-tasklist /nh /fi "ImageName eq LiveSplit.exe" 2>nul | find /i "LiveSplit.exe">nul
-if "%ERRORLEVEL%" NEQ "0" (
-  echo Starting LiveSplit, activate the WebSocket server before continuing!
-  start L:\LiveSplitDevBuild\LiveSplit.exe
-  pause
-)
+@REM tasklist /nh /fi "ImageName eq LiveSplit.exe" 2>nul | find /i "LiveSplit.exe">nul
+@REM if "%ERRORLEVEL%" NEQ "0" (
+@REM   echo Starting LiveSplit, activate the WebSocket server before continuing!
+@REM   start L:\LiveSplitDevBuild\LiveSplit.exe
+@REM   pause
+@REM )
 
 echo Starting music-stem-server...
 cd music-stem-server
 start cmd /k "npm run build && npm start"
-timeout /T 5 > nul
+timeout /T 10 > nul
 
 echo Starting electron-overlays...
 cd ../electron-overlays
