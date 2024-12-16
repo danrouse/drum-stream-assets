827b2f306345998475bf7c69e15615802240b3c0
Dan Rouse <dan@rouse.house>
2024-12-16 14:57:55 -0600

    update midi defs

--- shared/midiNoteDefinitions.ts
+++ shared/midiNoteDefinitions.ts
@@ -14,19 +14,19 @@ export interface MIDINoteDisplayDefinition extends MIDINoteDefinition {
 };
 
 export const midiNoteDefinitions: MIDINoteDefinition[] = [
-  { name: 'Snare', keys: [37, 38, 40], color: 'rgb(223, 25, 25)', z: 7 },
-  { name: 'Tom1', keys: [48, 50], color: 'rgb(94 120 231)', z: 6 },
-  { name: 'Tom2', keys: [45, 47], color: 'rgb(94 201 231)', z: 5 },
-  { name: 'Tom3', keys: [43, 58], color: 'rgb(77 218 134)', z: 4 },
-  { name: 'Tom4', keys: [41, 39], color: 'rgb(60 145 40)', z: 3 },
-  { name: 'HiHat', keys: [26, 46, 44], color: 'rgb(206 179 57)', z: 8 },
+  { name: 'Snare', keys: [37, 38, 40], color: 'rgb(223, 25, 25)', z: 2 },
+  { name: 'Tom1', keys: [48, 50], color: 'rgb(94 120 231)', z: 5 },
+  { name: 'Tom2', keys: [45, 47], color: 'rgb(94 201 231)', z: 4 },
+  { name: 'Tom3', keys: [43, 58], color: 'rgb(77 218 134)', z: 3 },
+  { name: 'Tom4', keys: [41, 39], color: 'rgb(60 145 40)', z: 2 },
+  { name: 'HiHat', keys: [26, 46, 44], color: 'rgb(206 179 57)', z: 7 },
   { name: 'Crash1', keys: [51, 53, 59], color: 'rgb(248 16 138)', z: 9 }, // plugged into ride
   { name: 'Crash2', keys: [52, 57], color: 'rgb(209 102 255)', z: 6 },
   { name: 'Crash3', keys: [49], color: 'rgb(109 102 255)', z: 9 },
   { name: 'Ride', keys: [29, 30], color: 'orangered', z: 5 },
-  { name: 'Ride2', keys: [31], color: 'brown', z: 6 },
-  { name: 'Splash', keys: [27, 28], color: '#aaa', z: 9 },
-  { name: 'Splash2', keys: [33], color: '#ccc', z: 8 },
+  { name: 'Ride2', keys: [31], color: 'brown', z: 7 },
+  { name: 'Splash', keys: [27, 28], color: '#aaa', z: 8 },
+  { name: 'Splash2', keys: [33], color: '#ccc', z: 6 },
   { name: 'Kick', keys: [36], color: '#444', z: 2 },
   // { name: 'Kick Secondary', keys: [36], color: '#444', z: 2 },
 ];
