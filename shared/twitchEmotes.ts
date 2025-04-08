5d22a860debf1cfb8b5679aa29aee4c0a5a49f93
Dan Rouse <dan@rouse.house>
2025-04-08 01:49:54 -0500

    don't throw an error when 7tv api is down

--- shared/twitchEmotes.ts
+++ shared/twitchEmotes.ts
@@ -16,12 +16,16 @@ interface EmoteData7tv {
   }
 }
 export async function load7tvEmotes(): Promise<{ [name: string]: string }> {
-  const blob = await fetch('https://7tv.io/v3/emote-sets/66f71cffc0ebe48adb092733');
-  const res = await blob.json();
-  const emotes = res.emotes.map((emoteData: EmoteData7tv) => [
-    emoteData.name, `https:${emoteData.data.host.url}/${emoteData.data.host.files[emoteData.data.host.files.length - 1].name}`
-  ]).reduce((acc: any, cur: any) => { acc[cur[0]] = cur[1]; return acc; }, {});
-  return emotes;
+  try {
+    const blob = await fetch('https://7tv.io/v3/emote-sets/66f71cffc0ebe48adb092733');
+    const res = await blob.json();
+    const emotes = res.emotes.map((emoteData: EmoteData7tv) => [
+      emoteData.name, `https:${emoteData.data.host.url}/${emoteData.data.host.files[emoteData.data.host.files.length - 1].name}`
+    ]).reduce((acc: any, cur: any) => { acc[cur[0]] = cur[1]; return acc; }, {});
+    return emotes;
+  } catch (err) {
+    return {};
+  }
 }
 
 let emoteURLs7tv: { [word: string]: string } | undefined;
