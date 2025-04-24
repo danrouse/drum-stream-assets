19aa7895c24bf2c4c0854010f4f80adc7e4eea76
Dan Rouse <dan@rouse.house>
2025-04-24 15:58:30 -0500

    jobs: yt-dlp use browser cookies, sanitize paths

--- job-handlers/getSongTags.ts
+++ job-handlers/getSongTags.ts
@@ -4,33 +4,53 @@ import { basename } from 'path';
 // @ts-expect-error
 import { parseFile } from 'music-metadata';
 import { parseTime } from '../shared/util';
+import { existsSync, readFileSync } from 'fs';
 
 export default async function getSongTags(songPath: string) {
   let tags: any = {};
   try {
     tags = await parseFile(songPath);
   } catch (e) {}
-  const res = await ffprobe(songPath, { path: ffprobeStatic.path });
-  let duration = 0;
-  if (res.streams[0].duration) {
-    duration = Number(res.streams[0].duration);
-  } else if (res.streams[0].tags.DURATION) {
-    duration = parseTime(res.streams[0].tags.DURATION);
+  try {
+    // most metadata can come from analyzing the media file directly with ffprobe
+    const res = await ffprobe(songPath, { path: ffprobeStatic.path });
+    // check for metadata downloaded from yt-dlp
+    // this allows sanitized filenames, and yt-dlp downloads are
+    // seldom well-tagged, so still try to get that info
+    const metadataPath = songPath.replace(/\..{3,}$/, '.info.json');
+    const hasMetadata = existsSync(metadataPath);
+    let duration = 0;
+    if (res.streams[0].duration) {
+      duration = Number(res.streams[0].duration);
+    } else if (res.streams[0].tags.DURATION) {
+      duration = parseTime(res.streams[0].tags.DURATION);
+    }
+    if (hasMetadata) {
+      const metadata = JSON.parse(readFileSync(metadataPath, 'utf-8'));
+      tags.common.artist = metadata.artist || '';
+      tags.common.title = metadata.fulltitle || metadata.track;
+      tags.common.album = `YouTube [${metadata.id}]`;
+      tags.common.track = { no: 1, of: 1 };
+    } else {
+      // legacy youtube filename parsing to get artist - title
+      const videoBasename = basename(songPath).replace(/^YouTube - /, '');
+      const videoTitle = videoBasename.substring(0, videoBasename.lastIndexOf('.'));
+      const youtubeIdMatch = videoTitle.match(/(.+)( \[\S{11}\])$/);
+      const album = `YouTube${youtubeIdMatch?.[2] || ''}`;
+      const titleWithoutId = youtubeIdMatch ? youtubeIdMatch[1] : videoTitle;
+      const hyphenParts = titleWithoutId.split(' - ');
+      const artist = hyphenParts.length > 1 ? hyphenParts[0] : '';
+      const title = hyphenParts.length > 1 ? hyphenParts.slice(1).join('-') : titleWithoutId;
+      if (!tags.common) tags.common = {};
+      tags.common.artist ||= artist.trim();
+      tags.common.title ||= title.trim();
+      tags.common.album ||= album.trim();
+      tags.common.track ||= { no: 1, of: 1 };
+    }
+    if (!tags.format) tags.format = {};
+    tags.format.duration ||= duration;
+    return tags;
+  } catch (e) {
+    console.log('song tagging error!', e);
   }
-  const videoBasename = basename(songPath).replace(/^YouTube - /, '');
-  const videoTitle = videoBasename.substring(0, videoBasename.lastIndexOf('.'));
-  const youtubeIdMatch = videoTitle.match(/(.+)( \[\S{11}\])$/);
-  const album = `YouTube${youtubeIdMatch?.[2] || ''}`;
-  const titleWithoutId = youtubeIdMatch ? youtubeIdMatch[1] : videoTitle;
-  const hyphenParts = titleWithoutId.split(' - ');
-  const artist = hyphenParts.length > 1 ? hyphenParts[0] : '';
-  const title = hyphenParts.length > 1 ? hyphenParts.slice(1).join('-') : titleWithoutId;
-  if (!tags.common) tags.common = {};
-  tags.common.artist ||= artist.trim();
-  tags.common.title ||= title.trim();
-  tags.common.album ||= album.trim();
-  tags.common.track ||= { no: 1, of: 1 };
-  if (!tags.format) tags.format = {};
-  tags.format.duration ||= duration;
-  return tags;
 }
