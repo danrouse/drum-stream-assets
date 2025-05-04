a086bfad5385865eb6330cf043c598eb74bae0b6
Dan Rouse <dan@rouse.house>
2025-05-04 16:21:53 -0500

    tts: remove backslashes
    streamerbot adds extra backslashes to the input, DOS escaping persists any unnecessary ones so manually remove them

--- tts.py
+++ tts.py
@@ -74,4 +74,5 @@ async def text_to_speech(text, voice=voices[random.randint(0, len(voices) - 1)])
 if __name__ == "__main__":
     NON_BMP_RE = re.compile(u"[^\U00000000-\U0000d7ff\U0000e000-\U0000ffff]", flags=re.UNICODE)
     stripped_text = NON_BMP_RE.sub(u'', sys.argv[1])
+    stripped_text = stripped_text.replace('\\', '')
     asyncio.run(text_to_speech(stripped_text[0:MAX_LEN]))
