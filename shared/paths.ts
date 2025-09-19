2eb4fec50898ce0d276d6b061f85d1249c61ba15
Dan Rouse <dan@rouse.house>
2025-09-19 13:38:56 -0500

    allow overwriting music library paths using env vars

--- shared/paths.ts
+++ shared/paths.ts
@@ -1,10 +1,14 @@
-import { join, dirname } from 'path';
+import { join, dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 export const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export const DOWNLOADS_PATH = join(__dirname, '..', 'music-stem-server', 'library', 'downloads');
-export const DEMUCS_OUTPUT_PATH = join(__dirname, '..', 'music-stem-server', 'library', 'separated');
+export const DOWNLOADS_PATH = process.env.DOWNLOADS_PATH
+  ? resolve(process.env.DOWNLOADS_PATH)
+  : join(__dirname, '..', 'music-stem-server', 'library', 'downloads');
+export const DEMUCS_OUTPUT_PATH = process.env.DEMUCS_OUTPUT_PATH
+  ? resolve(process.env.DEMUCS_OUTPUT_PATH)
+  : join(__dirname, '..', 'music-stem-server', 'library', 'separated');
 // this points to htdemucs directly - we've changed the model to htdemucs_ft
 // however htdemucs_ft is symlinked to the original htdemucs,
 // so all stems are still stored in the htdemucs directory
